f4765f9dc84d465a4fd20876a17adbc17b136139
vemi <ve@vemic.net>
2021-04-12 01:19:07 +0900

    WIP

---
+++ src/main/java/jp/vemi/mirel/apps/selenade/dto/yml/ArSelenadePage.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright(c) 2015-2021 vemi/mirelplatform.
+ */
+package jp.vemi.mirel.apps.selenade.dto.yml;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ArSelenadePage {
+    String id;
+    String name;
+    String appId;
+    String contextPath;
+    String notes;
+    List<Action> actions;
+
+    @Getter
+    @Setter
+    static class Action {
+        String id;
+        String name;
+        String type;
+        String locator;
+        Boolean ignoreIfNotFound;
+        List<String> includeTags;
+    }
+
+}
